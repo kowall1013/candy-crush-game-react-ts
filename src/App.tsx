89549f83ae09524fcdc9ba65e045ad8c18832bea
mateusz.kowalski <mateusz.kowalski@trustmate.io>
2022-01-14 07:06:42 +0100

    worging on logic

--- src/App.tsx
+++ src/App.tsx
@@ -32,17 +32,48 @@ function App():JSX.Element {
   }
 
   function checkColumnOfFour() {
-    for (let i = 0; i < 39; i++) {
-      const columnOfFour = [ i, i + width, i + width * 2, i + width * 3 ]
-      const currentColorOfCircle = currentBoardOfColors[i]
+    for (let i = 0; i <= 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const currentColor = currentBoardOfColors[i];
 
-     if(columnOfFour.every((circle) => currentBoardOfColors[circle] === currentColorOfCircle)) {
-       columnOfFour.forEach((circle) => currentBoardOfColors[circle] = 'white')
-     }
+      if(columnOfFour.every((circle) => currentBoardOfColors[circle] === currentColor)) {
+        columnOfFour.forEach((cirlce) => currentBoardOfColors[cirlce] = 'white')
+      }
     }
   }
 
-  checkColumnOfFour()
+  function checkRowOfFour() {
+    for(let i = 0; i <=64; i++) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3]
+      const currentColor = currentBoardOfColors[i]
+      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 61, 62, 63];
+
+      if(notValid.includes(i)) continue
+
+      if(rowOfFour.every((circle) => currentBoardOfColors[circle] === currentColor)){
+        rowOfFour.forEach((circle) => currentBoardOfColors[circle] = 'white')
+      }
+    }
+  }
+
+  function checkColumnOfThree() {
+    for(let i = 0; i <= 47; i++ ) {
+      const columnOfThree = [i, i + width, i + width * 2]
+      const currentColor = currentBoardOfColors[i]
+
+      if(columnOfThree.every((circle) => currentBoardOfColors[circle] === currentColor)) {
+        columnOfThree.forEach((circle) => currentBoardOfColors[circle] = 'white')
+      }
+    }
+  }
+
+  function checkRowOfThree() {
+    
+  }
+
+
+
+  checkColumnOfThree()
 
   useEffect(() => {
     createBoard()
@@ -56,3 +87,4 @@ function App():JSX.Element {
 }
 
 export default App;
+
